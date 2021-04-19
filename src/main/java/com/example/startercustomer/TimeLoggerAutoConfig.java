f2dc28ac2b2d05ac0e1819a46bb25bd1f786dfd5
alex <licerlee@gmail.com>
2021-04-19 17:14:44 +0800

    spring boot autoconfig demo

--- src/main/java/com/example/startercustomer/TimeLoggerAutoConfig.java
+++ src/main/java/com/example/startercustomer/TimeLoggerAutoConfig.java
@@ -2,17 +2,20 @@ package com.example.startercustomer;
 
 import com.example.startercustomer.properties.TimeLoggerProperties;
 import com.example.startercustomer.service.TimeLoggerService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@Import(TimeLoggerProperties.class)
 @EnableConfigurationProperties(TimeLoggerProperties.class)
+@ConditionalOnClass(TimeLoggerService.class)
+//@ConditionalOnProperty(prefix = "time.logger", name = "enable", havingValue = "true")
 public class TimeLoggerAutoConfig {
 
-    final TimeLoggerProperties timeLoggerProperties;
+    private final TimeLoggerProperties timeLoggerProperties;
 
     public TimeLoggerAutoConfig(TimeLoggerProperties timeLoggerProperties) {
         this.timeLoggerProperties = timeLoggerProperties;
@@ -20,6 +23,7 @@ public class TimeLoggerAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean(TimeLoggerService.class)
+    @ConditionalOnProperty(prefix = "time.logger", name = "enable", havingValue = "true")
     public TimeLoggerService timeLoggerService(){
         return new TimeLoggerService(timeLoggerProperties);
     }
--- src/main/java/com/example/startercustomer/properties/TimeLoggerProperties.java
+++ src/main/java/com/example/startercustomer/properties/TimeLoggerProperties.java
@@ -1,17 +1,14 @@
 package com.example.startercustomer.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "time.logger")
 public class TimeLoggerProperties {
 
+    private Boolean enable = true;
     private String dateFormatter;
-
-    public String getDateFormatter() {
-        return dateFormatter;
-    }
-
-    public void setDateFormatter(String dateFormatter) {
-        this.dateFormatter = dateFormatter;
-    }
 }
